c286829b1546aa5e487178185812ba32c7066a10
JessesCUkaegbu <ukaegbujesses@gmail.com>
2024-03-13 22:40:11 +0000

    another commit is made

--- diggyWeb/diggyWeb/settings.py
+++ diggyWeb/diggyWeb/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+g7r2vbqwl179ry4ff+t)^gha-*olz1(f)=4-zi5^v1!j02#$8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
