4320c0191514a6f30b51d86a01970e795018d3e2
JessesCUkaegbu <ukaegbujesses@gmail.com>
2024-03-13 23:02:03 +0000

    another commit is made

--- diggyWeb/diggyWeb/settings.py
+++ diggyWeb/diggyWeb/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile');
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Dwebsite/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile');
+
 
 
 # Default primary key field type
